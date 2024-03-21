3d1096b5c3f54455668a84bd9a564e5eac7c1f1a
Mojzi BlathyPC <mojzer.vince@blathy.info>
2024-03-21 08:43:43 +0100

    js ségeskedés

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("NIGA!");
+//alert("NIGA!");
+
+//DOM - Documentum Object Model, memóriában építi fel a weboldalt
+
+//document - egész oldalra vonatkozik
+//getElementById - id alapján kérjük le az adott objektumot
+//let +változónév - változó létrehozás
+let f_div = document.getElementById("first")
+//console.log - kiíratás a konzolra
+console.log(f_div.className)
+f_div.className = "";
+console.log(f_div.className)
