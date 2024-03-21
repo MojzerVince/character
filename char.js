201aee0006bba8ed175f644201d48e17fc700be0
Mojzi BlathyPC <mojzer.vince@blathy.info>
2024-03-21 12:08:29 +0100

    MUKODO JS GECC

---
+++ char.js
@@ -0,0 +1,20 @@
+function armor1Click(item){
+    let itemSRC = item.getAttribute("src");
+    let armor1 = document.querySelector("#armor1");
+    armor1.setAttribute("src", itemSRC);
+}
+function armor2Click(item){
+    let itemSRC = item.getAttribute("src");
+    let armor2 = document.querySelector("#armor2");
+    armor2.setAttribute("src", itemSRC);
+}
+function armor3Click(item){
+    let itemSRC = item.getAttribute("src");
+    let armor3 = document.querySelector("#armor3");
+    armor3.setAttribute("src", itemSRC);
+}
+function armor4Click(item){
+    let itemSRC = item.getAttribute("src");
+    let armor4 = document.querySelector("#armor4");
+    armor4.setAttribute("src", itemSRC);
+}
